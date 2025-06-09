f3ab2eedc61122c95364620e2ede180b1493ad8b
João Loureiro <admin@joaoloureiro.dev.br>
2025-06-09 14:39:50 -0300

    fix: update script path in PM2 configuration for portfolio backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'portfolio-backend', // A name for your application in PM2
-    script: 'server.js',      // The path to the script PM2 will run
+    script: 'backend/server.js',      // The path to the script PM2 will run
     instances: 1,             // Run a single instance of the application
     autorestart: true,        // Automatically restart if the app crashes
     watch: false,             // Do NOT watch for file changes in production
